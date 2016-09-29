2b708a735f6a4ea9f1ebc4c46dff0113b8bbf5b2
manoharthorat <manoharthorat725@gmail.com>
2016-09-29 17:58:01 +0530

    Add Progs

--- Assignment2/Program14/delete_empty_file.c
+++ Assignment2/Program14/delete_empty_file.c
@@ -1,18 +1,19 @@
 /* 14. Write program which accept directory name from user and delete all empty files from that directory.  */
-
-
 #include<stdio.h>
 #include<fcntl.h>
 #include<dirent.h>
 #include<unistd.h>
 #include<string.h>
+#include<sys/stat.h>
 
 int main(int argc, char * argv[])
 {
 	DIR *dir;
 	struct dirent * entry;
-		
-	if(argc!=3)
+	struct stat fstat;
+
+	char name[256]={'\0'};
+	if(argc!=2)
 	{
 		printf("Usage : ExeName SourceDirectory DestinationDirectory\n");
 		return -1;
@@ -22,8 +23,23 @@ int main(int argc, char * argv[])
 		printf("Unable to open the directory \n");
 		return -1;
 	}
-	while((entry=readdir(dir))!=NULL)
+	while( (entry=readdir(dir)) !=NULL)
 	{
-			
+		sprintf(name,"%s/%s",argv[1],entry->d_name);
+		stat(name,&fstat);
+		if(S_ISREG(fstat.st_mode))
+		{
+			if(((int)fstat.st_size)==0)
+			{
+				remove(name);
+			}			
+		}	
+		memset(&fstat,0,sizeof(fstat));	
+	}
+	closedir(dir);
+
+return 0;
+}
+
 
 
