b63a82f302f45706efc23f3c7a7e56c3bea6a12c
manoharthorat <manoharthorat725@gmail.com>
2016-10-17 23:37:37 +0530

    Add

--- Assignment4/Program26/hole.c
+++ Assignment4/Program26/hole.c
@@ -3,4 +3,26 @@
 #include<fcntl.h>
 
 int main(int argc, char * argv[])
+{
+	int fd;
+	char buff[50]={' '};
+	if(argc!=2)
+	{
+		printf("Usage : ExeName FileName\n");	
+		return 0;
+	}
+	fd=open(argv[1],O_WRONLY);
+	if(fd==-1)
+	{
+		printf("Unable to open the file \n");
+		return -1;
+	}
+	lseek(fd,1024,SEEK_END);
+	
+	write(fd,buff,1);
+	
+	close(fd);
 
+
+	return 0;
+}
