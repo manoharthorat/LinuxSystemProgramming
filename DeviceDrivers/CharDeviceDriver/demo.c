8a95bb86a99ff9e8e22d73f5d7a9201788c6edf7
manoharthorat <manoharthorat725@gmail.com>
2016-12-21 17:41:09 +0530

    driver ready

---
+++ DeviceDrivers/CharDeviceDriver/demo.c
@@ -0,0 +1,52 @@
+/*This is a general c program to send input to device*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<string.h>
+#include<unistd.h>
+
+#define BUFFER_LENGTH 256
+static char receive[BUFFER_LENGTH];
+int main()
+{
+	int ret,fd;
+	char stringToSend[BUFFER_LENGTH];
+	
+	printf("Starting device test code example.....\n");
+	fd=open("/dev/Character_Driver_1",O_RDWR);
+	
+	if(fd<0)
+	{
+		perror("Failed to open the device.....");
+		return errno;
+	}
+	printf("Type any short string to send to the kernel module ... \n");
+	scanf("%[^\n]%*c",stringToSend);
+	
+	printf("Writing message to the device[%s]\n",stringToSend);
+	ret=write(fd,stringToSend,strlen(stringToSend));
+	if(ret < 0 )
+	{
+		perror("Failed to write the message to the device.");
+		return errno;
+	}	
+	printf("Press ENTER to press back from the device.... \n");
+	getchar();
+	printf("Reading from the device....\n");
+	
+	ret=read(fd,receive,BUFFER_LENGTH);
+	if(ret<0)
+          {
+                  perror("Failed to read the message from device....");
+                  return errno;
+          }
+
+	printf("The received message is [%s]\n",receive);
+	printf("End of the program\n");
+
+
+return 0;
+}
+
